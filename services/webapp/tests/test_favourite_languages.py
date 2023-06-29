599d2a1cdffb2a696cec1209114b2ebb190ab894
Jana Sergienko <jana_monkey@mail.ru>
2023-06-29 10:53:21 +0300

    timeout

--- services/webapp/tests/test_favourite_languages.py
+++ services/webapp/tests/test_favourite_languages.py
@@ -2,7 +2,7 @@ import requests
 
 
 def test_favourite_languages() -> None:
-    rs = requests.get("http://localhost:8000/api/v1/favourite_languages")
+    rs = requests.get("http://localhost:8000/api/v1/favourite_languages", timeout=10)
     assert rs.json() == {
         "jen": "python",
         "sarah": "c",
